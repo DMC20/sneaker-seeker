9281f802eb9d51cbfb1cadc1520ddf6070aae262
Sugar Shane <scrisostomo17@gmail.com>
2021-10-21 00:36:16 -0400

    working react copy

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,44 @@
 import React from "react";
 
 const Home = () => {
+    // const searchShoes = () => {
+        //create state for holding returned api data
+
+        //create state for holding our search field data
+
+
+        //create state for holding saved shoeID values
+
+
+        //set up useEffect hook to save 'saved
+    // }
+
+
+    //create method to search for shoes and set state on form submit
+    // const handleFormSubmit = async (event) => {
+    //     event.preventDefault();
+
+    //     if (!searchInput) {
+    //         return false;
+    //     }
+
+    //     try {
+    //         const response = await fetch('this is where the API goes');
+            
+    //         if ()
+    //     }
+    // };
+
 
     return (
         <div>
             <h1>This is the home page </h1>
+            
+            <h1>Search for your favorite shoes!</h1>
+             
         </div>
+
+        
     )
 };
 
