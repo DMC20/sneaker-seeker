7785554a3f847e6cd7ce34ede1949423bc12c354
Spenny Berks <sberkebile@live.com>
2021-10-22 13:50:15 -0400

    API working on website

--- client/src/components/ShoeSearch/index.js
+++ client/src/components/ShoeSearch/index.js
@@ -4,32 +4,33 @@ class SearchShoes extends React.Component  {
     constructor(props) {
         super(props);
         this.state = {
-            shoes: []
+            shoes: ''
         };
 
-        this.create = this.create.bind(this);
+        this.create = this.shoeSearch.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    componentDidMount() {
-        // creates entity
+    shoeSearch() {
         fetch("https://the-sneaker-database.p.rapidapi.com/search?limit=20&query=vans", {
             "method": "GET",
             "headers": {
             "x-rapidapi-host": "the-sneaker-database.p.rapidapi.com",
-            "x-rapidapi-key": "90010f48fcmsh67c1ef52e0aacbep1ab420jsn8d9992b34041"
-        }
-    })
-    .then(response => response.json())
-    .then(response => {
-        this.setState({
-            shoes: response
+            "x-rapidapi-key": "d2ed8dea59msh9a4992883c5f9e0p194de4jsn78aad5a8e251"
+            }
         })
-        console.log(response)
-    })
-    .catch(err => {
-        console.log(err);
-    });
+        .then(response => response.json())
+        .then(response => {
+            this.setState({
+                shoes: response
+            })
+            console.log(response)
+            console.log(response.results)
+            console.log(this.state);
+        })
+        .catch(err => {
+            console.log(err);
+        });
     }
 
     handleChange(changeObject) {
@@ -38,7 +39,38 @@ class SearchShoes extends React.Component  {
 
     render() {
         return (
-            <div>{this.state.results}</div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <h1 className="display-4 text-center">Shoe search</h1>
+                        <form className="d-flex flex-column">
+                            <button className="btn btn-primary" type='button' onClick={(e) => this.shoeSearch(e)}>
+                                Search
+                            </button>
+                                {this.state.shoes && this.state.shoes.results.map(shoe => {
+                                    return <div>
+                                    <div>
+                                        <img src={shoe.image.thumbnail} />
+                                        <h3 class="card-title">Brand: {shoe.brand}</h3>
+                                        <h4 class="card-title">Name: {shoe.name}</h4>
+                                        <h5 class="card-title">Estimated cost: ${shoe.estimatedMarketValue}</h5>
+                                        <h5 class="card-title">Color: {shoe.colorway}</h5>
+                                        <h5 class="card-title">Release date: {shoe.releaseDate}</h5>
+                                        <p class="card-title">Description: {shoe.story}</p>
+                                        <p class="card-title">Gender: {shoe.gender}</p>
+                                    </div>
+                                </div>
+                                    // <tr>
+                                    //     <td>{shoe.name}</td>
+                                    //     <td>{shoe.estimatedMarketValue}</td>
+                                    //     <td>{shoe.story}</td>
+                                    // </tr>
+                                    
+                                })}
+                        </form>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
